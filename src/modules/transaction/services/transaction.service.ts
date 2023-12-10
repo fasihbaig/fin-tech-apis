d536e2d054cccffb6644852fba4e7b08ee7e737e
fasihbaig <fasihbaig85@gmail.com>
2023-12-11 01:20:48 +0500

    Check account exist against logged in user or not

--- src/modules/transaction/services/transaction.service.ts
+++ src/modules/transaction/services/transaction.service.ts
@@ -1,11 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, ForbiddenException, Injectable } from '@nestjs/common';
 import { CreateTransactionDto } from '../dto/create-transaction.dto';
 import { InjectModel } from '@nestjs/sequelize';
 import { Transaction } from '../../../modules/database/entities';
 import { TransactionType } from '../../../enums';
 import { AccountService } from '../../../modules/account/services/account.service';
-import { Sequelize, Transaction as SequelizeTransaction } from 'sequelize';
+import { Transaction as SequelizeTransaction } from 'sequelize';
 import { ModuleRef } from '@nestjs/core';
+import { Sequelize } from 'sequelize-typescript';
 
 @Injectable()
 export class TransactionService {
@@ -20,17 +21,32 @@ export class TransactionService {
    * @param { Partial<CreateTransactionDto> } createTransactionDto 
    * @returns { Promise<Transaction> }
    */
-  addTransactionHandler(createTransactionDto: Partial<CreateTransactionDto>, userId: number): Promise<Transaction> {
+  addTransactionHandler(createTransactionDto: CreateTransactionDto, userId: number): Promise<Transaction> {
     const sequelize = this.moduleRef.get(Sequelize, {strict: false});
     const accountService = this.moduleRef.get(AccountService, {strict: false});
     return sequelize.transaction(async (transaction) => {
+       const accountDetails = await accountService.getUserIdAgainstAccountNumber(
+        createTransactionDto.accountId as number,
+        userId
+       );
+
+       if(!accountDetails) {
+        throw new ForbiddenException("You are not authorized to do transaction against given account");
+       }
+
+       if(
+        createTransactionDto.transactionType === TransactionType.DEBIT 
+        &&  (accountDetails.get("balance") - createTransactionDto.amount) < 0 ) {
+        throw new BadRequestException("You don not have sufficient balance.")
+       }
+
        const transactionAdded =  await this.addTransaction(
         createTransactionDto.accountId as number,
         createTransactionDto.amount as number,
         createTransactionDto.transactionType as TransactionType,
         transaction
        );
-       accountService.updateAccountBalance(
+       await accountService.updateAccountBalance(
         createTransactionDto.accountId as number,
         createTransactionDto.amount as number,
         createTransactionDto.transactionType as TransactionType,
