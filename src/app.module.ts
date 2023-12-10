62061f7921c15c267f2cd3eaf1c55eb9820ad1cb
fasihbaig <fasihbaig85@gmail.com>
2023-12-10 12:40:25 +0500

    Updated: aap module to use config with global configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true })
+  ],
+  controllers: [
+    AppController
+  ],
+  providers: [
+    AppService,
+    ConfigService
+  ],
 })
 export class AppModule {}
