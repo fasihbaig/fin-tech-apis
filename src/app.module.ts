94cd5cbd1e9d3ffb7e78ff4900fa7a79d514515e
fasihbaig <fasihbaig85@gmail.com>
2023-12-10 14:38:45 +0500

    Updated: app module import for sequelize integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,25 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { getModels } from './modules/database/entities';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true })
+    ConfigModule.forRoot({ isGlobal: true }),
+    SequelizeModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        dialect: configService.get("DB_DIALECT"),
+        host: configService.get("DB_HOST"),
+        port: configService.get("DB_PORT"),
+        username: configService.get("DB_USERNAME"),
+        password: configService.get("DB_PASSWORD"),
+        database: configService.get("DB_DATABASE"),
+        models: [...getModels()],
+      }),
+      inject: [ConfigService],
+    }),
   ],
   controllers: [
     AppController
