abb6a4eeba72c66661f73481c2b7b96bcb07d00a
fasihbaig <fasihbaig85@gmail.com>
2023-12-10 18:40:56 +0500

    Updated: user create endpoint to hash password and return authentication token after user creation

--- src/modules/user/services/user.service.ts
+++ src/modules/user/services/user.service.ts
@@ -4,23 +4,40 @@ import { UpdateUserDto } from '../dto/update-user.dto';
 import { InjectModel } from '@nestjs/sequelize';
 import { User } from '../../database/entities';
 import { Op } from 'sequelize';
+import { JwtAuthService } from '../../../modules/common/jwt-auth/jwt-auth.service';
+import { TextHashManager } from '../../../modules/common/hash/text-hash-manager';
 
 @Injectable()
 export class UserService {
 
   constructor( 
-    @InjectModel(User) private userModel: typeof User
+    @InjectModel(User) private userModel: typeof User,
+    private jwtAuthService: JwtAuthService,
+    private textHashManager: TextHashManager
   ) {}
 
   /**
    * 
    * @param { Partial<CreateUserDto> } createUserDto 
-   * @returns { Promise<User> }
+   * @returns { Promise<{ user: User, token: string }> }
    */
-  create(
+  async createUserHandler(
     createUserDto: Partial<CreateUserDto>
-  ): Promise<User> {
-    return this.userModel.create(createUserDto);
+  ): Promise<{
+    user:User,
+    token: string
+  }> {
+    const passwordHash = await this.textHashManager.createTextHash(createUserDto.password as string, 10);
+    createUserDto.password = passwordHash;
+    const user = await this.userModel.create(createUserDto);
+    const token = await this.jwtAuthService.createNewToken({
+        id: user.get("id"),
+        username: user.get("username"),
+        firstName: user.get("firstName"),
+        lastName: user.get("lastName"),
+        email: user.get("email")
+    })
+    return { user, token }
   }
 
   /**
--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -2,10 +2,20 @@ import { Module } from '@nestjs/common';
 import { UserService } from './services/user.service';
 import { UserController } from './controllers/user.controller';
 import { DatabaseModule } from '../database/database.module';
+import { CommonModule } from '../common/common.module';
+import { JwtAuthService } from '../common/jwt-auth/jwt-auth.service';
+import { TextHashManager } from '../common/hash/text-hash-manager';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [
+    DatabaseModule,
+    CommonModule
+  ],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [
+    UserService,
+    JwtAuthService,
+    TextHashManager
+  ],
 })
 export class UserModule {}
